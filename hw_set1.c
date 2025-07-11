f598ae262414e7747c7ba827fc6aeb7446b85057
rahulPandya-280507 <rahulpandya280507@gmail.com>
2025-07-11 23:11:57 +0530

    hw_set1 Q1

---
+++ hw_set1.c
@@ -0,0 +1,16 @@
+/*
+Q1] Write a program to calculate perimeter of rectangle. Take sides, a & b, from the user.
+Q2] Take a number(n) from user & output its cube(n*n*n).
+*/
+
+//Q1
+#include <stdio.h>
+int main(){
+    int a,b,perimeter;
+    printf("Enter sides a and b of the rectangle: ");
+    scanf("%d %d", &a, &b);
+    // printf("%d, %d", a, b);
+    perimeter = 2*a + 2*b;
+    printf("The perimeter of the rectangle with sides {%d} and {%d} is: %d units", a,b,perimeter);
+    return 0;
+}
