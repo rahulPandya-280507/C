587462f76ddd0cf5d1636cf2f4bfc788b440bad5
rahulPandya-280507 <rahulpandya280507@gmail.com>
2025-07-11 23:14:47 +0530

    this is the 2nd Q from hw_set1

--- hw_set1.c
+++ hw_set1.c
@@ -4,7 +4,7 @@ Q2] Take a number(n) from user & output its cube(n*n*n).
 */
 
 //Q1
-#include <stdio.h>
+/*#include <stdio.h>
 int main(){
     int a,b,perimeter;
     printf("Enter sides a and b of the rectangle: ");
@@ -13,4 +13,13 @@ int main(){
     perimeter = 2*a + 2*b;
     printf("The perimeter of the rectangle with sides {%d} and {%d} is: %d units", a,b,perimeter);
     return 0;
+}*/
+
+//Q2
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("The cube of {%d} is: %d", n, n*n*n);
 }
